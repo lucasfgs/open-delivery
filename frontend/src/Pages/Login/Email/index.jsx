df81b6da55e5ccf3823e107d35c9a745895d2a8c
Lucas Ferreira <blooodcf@gmail.com>
2019-12-21 20:06:41 -0300

    Customer e-mail Authentication

--- frontend/src/Pages/Login/Email/index.jsx
+++ frontend/src/Pages/Login/Email/index.jsx
@@ -1,15 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Container } from "./styles";
+import api from "../../../services/api";
 
-export default function() {
+export default function({ history }) {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  const [error, setError] = useState(false);
+
+  const handleChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    console.log(user);
+    let response = await api.post("/auth/customers", {
+      email: user.email,
+      password: user.password
+    });
+
+    const { id } = response.data;
+    if (id) {
+      localStorage.setItem("userId", id);
+      history.push("/dashboard");
+    } else setError(true);
+
+    console.log(error);
+
+    console.log(response.data);
+  };
   return (
     <Container>
       <div className="login-form">
-        <h2 className="form--title">Informe o seu e-mail para continuar</h2>
-        <input className="form--input" type="text" placeholder="E-mail" />
-        <input className="form--input" type="password" placeholder="Senha" />
-        <button className="btn form--btn">Continuar</button>
+        <h2 className="form--title">Informe os seus dados</h2>
+        <input
+          className="form--input"
+          type="text"
+          name="email"
+          placeholder="E-mail"
+          onChange={handleChange}
+        />
+        <input
+          className="form--input"
+          type="password"
+          name="password"
+          placeholder="Senha"
+          onChange={handleChange}
+        />
+        <button className="btn form--btn" onClick={handleSubmit}>
+          Continuar
+        </button>
       </div>
     </Container>
   );
